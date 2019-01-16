94c6cc8a65e76424728967249edffc222220db2a
fran from aa <zhanfan930818@hotmail.com>
2019-01-16 18:17:58 +0800

    添加配置扩展方法

---
+++ src/LogDashboard/Extensions/LogDashboardOptionsExtension.cs
@@ -0,0 +1,27 @@
+namespace LogDashboard.Extensions
+{
+    public static class LogDashboardOptionsExtension
+    {
+        /// <summary>
+        /// 设置根目录
+        /// </summary>
+        /// <param name="options">LogDashboardOptions</param>
+        /// <param name="rootPath">日志根目录哦</param>
+        public static void SetRootPath(this LogDashboardOptions options,string rootPath)
+        {
+            options.RootPath=rootPath;
+        }
+
+        /// <summary>
+        /// 设置分隔符
+        /// </summary>
+        /// <param name="options">LogDashboardOptions</param>
+        /// <param name="startDelimiter">分隔符</param>
+        /// <param name="endDelimiter">结束分隔符</param>
+        public static void SetDelimiter(this LogDashboardOptions options,string startDelimiter="||",string endDelimiter="||end")
+        {
+            options.FileEndDelimiter=startDelimiter;
+            options.FileFieldDelimiter=endDelimiter;
+        }
+    }
+}
