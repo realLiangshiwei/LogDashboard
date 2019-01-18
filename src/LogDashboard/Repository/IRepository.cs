784c6d7c70209f1d4f7e53ae73caf2e2396be252
liangshiwei <deawback@hotmail.com>
2019-01-18 09:44:05 +0800

    Resolved #19 : Implement Async Repository

--- src/LogDashboard/Repository/IRepository.cs
+++ src/LogDashboard/Repository/IRepository.cs
@@ -2,19 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using DapperExtensions;
 
 namespace LogDashboard.Repository
 {
     public interface IRepository<T> where T : class, ILogModel
     {
-        T FirstOrDefault(Expression<Func<T, bool>> predicate = null);
+        Task<T> FirstOrDefault(Expression<Func<T, bool>> predicate = null);
 
-        IEnumerable<T> GetList(Expression<Func<T, bool>> predicate = null);
+        Task<IEnumerable<T>> GetList(Expression<Func<T, bool>> predicate = null);
 
-        int Count(Expression<Func<T, bool>> predicate = null);
+        Task<int> Count(Expression<Func<T, bool>> predicate = null);
 
-        IEnumerable<T> GetPageList(int page, int size, Expression<Func<T, bool>> predicate = null, params ISort[] sorts);
+        Task<IEnumerable<T>> GetPageList(int page, int size, Expression<Func<T, bool>> predicate = null, params ISort[] sorts);
 
     }
 
