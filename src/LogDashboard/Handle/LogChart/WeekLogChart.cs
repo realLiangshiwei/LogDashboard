ac6184162a9fb275aea37f2f630862e5c6aebcfd
liangshiwei <deawback@hotmail.com>
2019-01-20 18:03:49 +0800

    fix bugs

--- src/LogDashboard/Handle/LogChart/WeekLogChart.cs
+++ src/LogDashboard/Handle/LogChart/WeekLogChart.cs
@@ -11,20 +11,22 @@ namespace LogDashboard.Handle.LogChart
         {
             var now = DateTime.Now;
             var dayOfWeek = (int)now.DayOfWeek;
+            dayOfWeek = dayOfWeek == 0 ? 6 : dayOfWeek == 6 ? 0 : dayOfWeek;
             var output = new GetLogChartsOutput(7);
             for (var i = 0; i < 7; i++)
             {
-                if (i > dayOfWeek || (i != dayOfWeek && dayOfWeek == 0))
+                if (i > dayOfWeek)
                 {
-                    output.All[i == 0 ? 6 : i == 6 ? 0 : i] = 0;
-                    output.Error[i == 0 ? 6 : i == 6 ? 0 : i] = 0;
+                    output.All[i] = 0;
+                    output.Error[i] = 0;
                 }
                 else
                 {
-                    var day = now.AddDays(i - dayOfWeek);
+  
+                    var day = now.AddDays(0 - (dayOfWeek - i));
                     var weeHours = now.AddDays(i - dayOfWeek).Date.AddHours(23).AddMinutes(59);
-                    output.All[i == 0 ? 6 : i == 6 ? 0 : i] = await repository.Count(x => x.LongDate >= day.Date && x.LongDate <= weeHours);
-                    output.Error[i == 0 ? 6 : i == 6 ? 0 : i] = await repository.Count(x => x.LongDate >= day.Date && x.LongDate <= weeHours && x.Level == "ERROR");
+                    output.All[i] = await repository.Count(x => x.LongDate >= day.Date && x.LongDate <= weeHours);
+                    output.Error[i ] = await repository.Count(x => x.LongDate >= day.Date && x.LongDate <= weeHours && x.Level == "ERROR");
                 }
             }
 
