3c0cdc7f602e5ef854dd887e6d8e0d99fb1f1c12
Bryan <1029442928@qq.com>
2023-04-22 23:24:23 +0800

    更新Token封装

--- src/LogDashboard/Authorization/Filters/LogdashboardAccountAuthorizeFilter.cs
+++ src/LogDashboard/Authorization/Filters/LogdashboardAccountAuthorizeFilter.cs
@@ -43,8 +43,8 @@ namespace LogDashboard
                     time <= DateTime.Now.ToUnixTimestamp() &&
                     time > DateTime.Now.Add(-CookieOptions.Expire).ToUnixTimestamp())
                 {
-                    var tokenValue = $"{CookieOptions.Secure(this)}&&{timestamp}".ToMD5();
-                    isValidAuthorize = tokenValue == token;
+                    var vaildToken = GetToken(timestamp);
+                    isValidAuthorize = vaildToken == token;
                 }
             }
 
@@ -76,9 +76,14 @@ namespace LogDashboard
         public void SetCookieValue(HttpContext context)
         {
             var timestamp = DateTime.Now.ToUnixTimestamp().ToString();
-            var token = $"{CookieOptions.Secure(this)}&&{timestamp}".ToMD5();
+            var token = GetToken(timestamp);
             context.Response.Cookies.Append(CookieOptions.TokenKey, token, new CookieOptions() { Expires = DateTime.Now.Add(CookieOptions.Expire) });
             context.Response.Cookies.Append(CookieOptions.TimestampKey, timestamp, new CookieOptions() { Expires = DateTime.Now.Add(CookieOptions.Expire) });
+        }
+
+        private string GetToken(string timestamp)
+        {
+            return $"{CookieOptions.Secure(this)}&&{timestamp}".ToMD5();
         }
     }
 }
