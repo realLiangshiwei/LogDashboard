55228850e2f382b16ad7c8d89cbad80d6d9e36ef
gao-chao-2020 <515826435@qq.com>
2023-08-28 11:36:13 +0800

    修改等级枚举

--- src/LogDashboard/Models/LogLevel.cs
+++ src/LogDashboard/Models/LogLevel.cs
@@ -2,11 +2,11 @@
 {
     public class LogLevelConst
     {
-        public const string Info = "INFO";
+        public const string Info = "INFORMATION";
         public const string Error = "ERROR";
         public const string Debug = "DEBUG";
         public const string Trace = "TRACE";
-        public const string Warn = "WARN";
+        public const string Warn = "WARNING";
         public const string Fatal = "FATAL";
     }
 
